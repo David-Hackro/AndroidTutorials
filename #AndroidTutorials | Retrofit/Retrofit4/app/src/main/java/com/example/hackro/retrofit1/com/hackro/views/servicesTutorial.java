4636a05c80ade826392a0710ab07fc3e51e633ad
David Hackro <hackro91@gmail.com>
2017-01-17 21:36:49 -0600

    Se agregaron los comentarios

--- #AndroidTutorials | Retrofit/Retrofit4/app/src/main/java/com/example/hackro/retrofit1/com/hackro/views/servicesTutorial.java
+++ #AndroidTutorials | Retrofit/Retrofit4/app/src/main/java/com/example/hackro/retrofit1/com/hackro/views/servicesTutorial.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Response;
-import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
@@ -26,9 +25,5 @@ public interface servicesTutorial {
     //https://androidtutorials.herokuapp.com/findUser?id=1
     @GET("findUser")
     Call<ResponseService> findUserGet(@Query("id") int idUser);//Recuerda que el valor @Query(valor) debe ser igual a como lo espera el servicio
-    //https://androidtutorials.herokuapp.com/findUserPost
-    @FormUrlEncoded//Recuerda que si envias parametros en post,es necesario el encode
-    @POST("findUserPost")
-    Call<String> findUserPost(@Field("name")String nombre);//el nombre del parametro debe ser igual al que e servicio espera
 
 }

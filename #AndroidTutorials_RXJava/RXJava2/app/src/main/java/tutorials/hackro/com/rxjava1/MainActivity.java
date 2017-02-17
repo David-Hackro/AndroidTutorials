8cd48abe1769ea2d42ae5a443eaba2bc0fdfabf3
David Hackro <hackro91@gmail.com>
2017-02-17 09:45:41 -0600

    @DavidHackro

--- #AndroidTutorials_RXJava/RXJava2/app/src/main/java/tutorials/hackro/com/rxjava1/MainActivity.java
+++ #AndroidTutorials_RXJava/RXJava2/app/src/main/java/tutorials/hackro/com/rxjava1/MainActivity.java
@@ -27,19 +27,14 @@ public class MainActivity extends AppCompatActivity {
         userList.add(new User("Gael",18));
         userList.add(new User("Arturo",9));
         userList.add(new User("Iker",20));
+        userList.add(new User("Fabiola",15));
         userList.add(new User("Carolina",87));
         userList.add(new User("Sharon",2));
 
         /*
-        *  User{name='David', age=23}
-02-16 13:38:32.533 8175-8175/tutorials.hackro.com.rxjava1 E/User:: User{name='Paulina', age=21}
-02-16 13:38:32.534 8175-8175/tutorials.hackro.com.rxjava1 E/User:: User{name='Gael', age=18}
-02-16 13:38:32.534 8175-8175/tutorials.hackro.com.rxjava1 E/User:: User{name='Iker', age=20}
-02-16 13:38:32.534 8175-8175/tutorials.hackro.com.rxjava1 E/User:: User{name='Carolina', age=87}
-        * */
-        //From — convert some other object or data structure into an Observable
-        //From - convertir algún otro objeto o estructura de datos en un Observable
-        Observable.from(userList)
+                //English definition
+
+                //.distinct(user1 -> user1.getName())//suppress duplicate items
                 //.filter(user1 -> user1.getAge() >= 18) //Applies a filter depending on a condition
                 //.skip(1) //suppress n items (first)
                 //.skipLast(1)//suppress n items (last)
@@ -48,11 +43,24 @@ public class MainActivity extends AppCompatActivity {
                 //.takeLast(2) limit the number the results (last)
                 //.last() // return last item
                 //.first() // return first item
-                .subscribe(user -> Log.e("User : ",user.toString()));
+        */
+
+        //From — convert some other object or data structure into an Observable
+        //From - convertir algún otro objeto o estructura de datos en un Observable
+        Observable.from(userList)
+                //.distinct(user1 -> user1.getName())//suprime los items repetidos
+                //.filter(user1 -> user1.getAge() >= 18) //Aplica un filtro mediante una conficional
+                //.skip(1) //suprime los primeros (n) items (Inicio)
+                //.skipLast(1)//suprime los ultimo (n) items (Final)
+                //.count()//retorna el numero de items
+                //.take(2) //limita el numero de items (Inicio)
+                //.takeLast(2) //limita el numero de items (Final)
+                //.last() // retorna el ultimo item
+                //.first() // retorna el primer item
+                .subscribe(user -> Log.e("User: ",user.toString()));
         //subscribe: Las emisiones y las notificaciones de un Observable
         // subscribe:operate upon the emissions and notifications from an Observable
 
-        //Basicamente, en el from,indicamos la lista o estructura de datos,la cual sera
 
     }
 }
